d967d61d1345e9fe96b46675f98934b3801cf37d
Javier Carrillo <stringparser@gmail.com>
2014-09-08 19:27:12 +0200

    update readme

--- example/index.js
+++ example/index.js
@@ -1,23 +1,40 @@
 
+var is = require('./..');
+// some cool constructors over here
 var Stream = require('stream');
 var EventEmitter = require('events').EventEmitter;
-var is = require('./..');
 
-is( 0 )
+is( 0 ) ;
+// -> { number: '0' }
 is( 42 );
+// -> { number: 42 }
 is( NaN );
+// -> { number: 'NaN', nan: true }
 is( null );
+// -> { null: true }
 is( true );
+// -> { boolean: true, true: true }
 is( false );
+ // -> { boolean: 'false', false: true }
 is( Infinity );
+// -> { number: Infinity, infinity: true }
 is( undefined );
+// -> { undefined: true }
 is( 'a string');
+// -> { string: 'a string' }
 is( /a regex/ );
+// -> { object: true, regexp: /a regex/ }
 is( [ Math.E ] );
+// -> { object: true,  array: [ 2.718281828459045 ] }
 is( { type : 'toy' });
+// -> { object: { type: 'toy' } }
 is( new Date());
+// -> { object: true,  date: Mon Sep 08 2014 19:10:32 GMT+0200 (CEST) }
 is( new Error() );
+// -> { object: true, error: [Error] }
 is( new Stream() );
+// -> { object: true,  eventemitter: true,  stream: { domain: null,  _events: {}, _maxListeners: 10 } }
 is( new EventEmitter() );
+// -> { object: true,  eventemitter: { domain: null,     _events: {},  _maxListeners: 10 } }
 is( function(){ } );
-is( (function(){ return arguments; })() );
+// -> { object: true, function: [Function] }

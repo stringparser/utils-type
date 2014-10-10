716b360521c2155b8203556b4d51b1d06a660594
Javier Carrillo <stringparser@gmail.com>
2014-10-10 18:35:13 +0200

    now is nicer

--- index.js
+++ index.js
@@ -2,20 +2,15 @@
 
 var _ = { isPlainObject : require('lodash.isplainobject') };
 
-exports = module.exports = type;
-
 function type(what){
 
-  var leType = { types : getCtorName(what) };
   var strRep = what + '';
+  var leType = { types : getCtorName(what) };
   leType[leType.types] = what || !!strRep || ' ';
 
-  // pritives and arguments
-  if( (/undefined|null|string|number|boolean|arguments|symbol/).test(leType.types) ) {
-    if( leType.arguments ){
-      leType.object = what;
-      leType.types = 'object '+leType.types;
-    } else if( leType.number ){
+  // pritives
+  if( (/undefined|null|string|number|boolean|symbol/).test(leType.types) ) {
+    if( leType.number ){
       if( parseInt(strRep) === what ){
         leType.integer = what;
         leType.types += ' integer';
@@ -33,7 +28,7 @@ function type(what){
   }
 
   // everything else is an object
-  leType.object = true;
+  leType.object = what;
   if( _.isPlainObject(what) ){
     leType.plainObject = what;
     leType.types += ' plainObject';
@@ -41,17 +36,21 @@ function type(what){
     leType.types = 'object '+leType.types;
   }
 
-  var name, super_ = what.constructor.super_;
-  while(super_){ // util.inherits pattern
-    name = super_.name.toLowerCase();
-    leType[name] = true;
-    leType.types += ' '+name;
-    super_ = super_.constructor.super_;
+  // util.inherits pattern
+  if( what.constructor.super_ ){
+    var name, super_ = what.constructor.super_;
+    while(super_){
+      name = super_.name.toLowerCase();
+      leType[name] = true;
+      leType.types += ' '+name;
+      super_ = super_.constructor.super_;
+    }
+    name = super_ = null;
   }
-  name = super_ = null;
 
   return leType;
 }
+exports = module.exports = type;
 
 var __ = ({ });
 var __toString = __.toString;

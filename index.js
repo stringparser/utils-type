ab79e4367d066a5d7c15f050586402e0c8454518
Javier Carrillo <stringparser@gmail.com>
2015-03-29 16:32:57 +0200

    extracted type.match into a different method so it adds less overhead

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ function type(src){
   var that = { };
   var classType = util.getClassType(src);
   that[classType] = src || Boolean(src + '') || ' ';
-  
-  util.defineProperty(that, 'match', '', function matchType(pattern){
-    var types = Object.keys(this);
-    var source = this[types[0]];
-    if(RegExp(pattern).test(types.join(' '))){
-      return source || Boolean(source + '') || ' ';
-    }
-    return null;
-  });
 
   // primitives
   if(primitives.test(classType)) {
@@ -55,5 +46,13 @@ function type(src){
   }
   return that;
 }
+
+type.match = function typeMatch(src, pattern){
+  var types = Object.keys(type(src));
+  if(RegExp(pattern).test(types.join(' '))){
+    return src || Boolean(src + '') || ' ';
+  }
+  return null;
+};
 
 exports = module.exports = type;
